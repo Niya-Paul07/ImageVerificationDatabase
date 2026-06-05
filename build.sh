0015ed8bbdcba23f3af3f382b87f14e4c552a1ed
Anupa <anupasiby4@gmail.com>
2026-06-05 10:42:06 +0530

    simplify build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-pip install cmake
-pip install https://github.com/jloh02/dlib/releases/download/v19.22/dlib-19.22.99-cp39-cp39-linux_x86_64.whl
-pip install face_recognition
 pip install -r requirements.txt
